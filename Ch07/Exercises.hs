5e3cfdbe6bcc1169151c3572fa5f06dc34273389
Noxsense <noxsense@gmail.com>
2023-06-11 15:08:36 +0200

    Apply Lecture 07 (weird union), Lecture 08 Nat)

--- Ch07/Exercises.hs
+++ Ch07/Exercises.hs
@@ -1,5 +1,14 @@
 -- Chapter 07: Modules
-module Exercises where
+module Exercises
+ ( wordCount
+ , keySet
+ , union1
+ , union2
+ ) where
+
+import Data.Map (Map, keys)
+import Data.Set (Set, fromList)
+import Data.List (intersect, nub, delete)
 
 {-|
 Exercise: Using the functions you just learned about, implement a function that
@@ -14,8 +23,8 @@ Exercise: Using Data.Map and Data.Set, implement a function that converts a Map
 to a Set of keys.
 |-}
 
-keySet :: _
-keySet = _
+keySet :: Ord a => Map a b -> Set a
+keySet = fromList . keys
 
 {-|
 Exercise: Implement 'union' for lists using
@@ -23,11 +32,23 @@ Exercise: Implement 'union' for lists using
 (2) At most 'filter', intersect', (++) and 'nub'
 |-}
 
-union1 :: _
-union1 = _
+union1 :: Eq a => [a] -> [a] -> [a]
+union1 as [] = as
+union1 [] (bs) = distinct' bs
+union1 (a:as) (bs) = a : union1 as bs'
+  where
+    bs' = foldr (\i acc -> delete a (i : acc)) [] (distinct' bs)
 
-union2 :: _
-union2 = _
+-- (1) At most 'delete', (:), and 'foldr'
+-- not exported.
+distinct' :: Eq a => [a] -> [a]
+distinct' [] = []
+distinct' (x:xs) = x : distinct' (foldr (\i acc -> delete x (i:acc)) [] xs)
+
+union2 :: Eq a => [a] -> [a] -> [a]
+union2 as [] = as
+union2 [] (b:bs) = [b] ++ union2 [] (filter (/= b) bs) -- aka distinct''
+union2 as (b:bs) = as ++ union2 [] (filter (`notElem` as) bs)
 
 {-|
 Exercise: Export all functions you have defined in this module explicitly.
--- Ch08/Exercises.hs
+++ Ch08/Exercises.hs
@@ -7,7 +7,8 @@ Define a data type that represents this idea using a constructor for zero and
 one for the successor of a number.
 |-}
 
-data Nat = _
+data Nat = Zero | Succ Nat
+  deriving ( Show )
 
 {-|
 Exercise: Define 'plus', which acts recursively on two natural numbers and
@@ -15,7 +16,9 @@ returns their sum.
 |-}
 
 plus :: Nat -> Nat -> Nat
-plus = _
+plus Zero x = x
+plus (Succ Zero) x = Succ x -- +1
+plus (Succ a) b = Succ (plus a b) -- +n = +1 + 1 .. +1
 
 
 {-|
@@ -23,7 +26,10 @@ Exercise: Define an Eq instance for your Peano numbers.
 |-}
 
 instance Eq Nat where
-  _
+  (==) :: Nat -> Nat -> Bool
+  (==) Zero Zero = True
+  (==) (Succ b) Zero = False
+  (==) (Succ b) (Succ a) = a == b
 
 {-|
 Exercise: Define a class that allows casting to and from Int. Your functions
@@ -32,8 +38,17 @@ casting a negative integer to a natural number). Make an instance for your
 natural number type.
 |-}
 
-class _ where
-  _
+class FooBarInt a where
+  toInt :: a -> Int
+  fromInt :: Int -> a
 
-instance _ Nat where
-  _
+instance FooBarInt Nat where
+  toInt :: Nat -> Int
+  toInt Zero = 0
+  toInt (Succ a) = 1 + toInt a
+
+  fromInt :: Int -> Nat
+  fromInt i
+    | i == 0 = Zero
+    | i > 0 = Succ (fromInt (i-1))
+    | otherwise = undefined
